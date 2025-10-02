aae0d50095a00f6e2f4faeb329e0bf1cb2331e93
itsVinM <vincentiu.mocanu00@gmail.com>
2025-10-02 12:06:07 +0200

    updated graph

--- TRACKER.py
+++ TRACKER.py
@@ -111,7 +111,7 @@ class ValidationTracker:
         fig.add_trace(go.Bar(name='Total Requests', x=['Total Requests'], y=[total_requests], marker_color='grey'))
 
         # Homologation status (right axis)
-        fig.add_trace(go.Bar(name='Approved', x=['Homologation'], y=[homologation_counts.get('Approved', 0)], marker_color='lightgreenn', yaxis='y2'))
+        fig.add_trace(go.Bar(name='Approved', x=['Homologation'], y=[homologation_counts.get('Approved', 0)], marker_color='lightgreen', yaxis='y2'))
         fig.add_trace(go.Bar(name='Rejected', x=['Homologation'], y=[homologation_counts.get('Rejected', 0)], marker_color='salmon', yaxis='y2'))
 
         # Layout
