43d8b736e4c9545fdcc5064c88584bb06a23a47e
itsVinM <vincentiu.mocanu00@gmail.com>
2025-10-16 10:26:59 +0200

    missing func

--- TRACKER.py
+++ TRACKER.py
@@ -125,6 +125,7 @@ def project_tracker():
 
     with tab1:
         but1, but2 = st.columns(2)
+
         metric1, metric2, metric3, metric4, metric5, metric6, metric7= st.columns(7)
         
         col_request, col_product, col_component, col_homologation = st.columns(4)
@@ -176,7 +177,7 @@ def project_tracker():
             "🛠️FUNCTION", "📡 EMC RADIATED", "⚡ EMC CONDUCTED"
         ])])
 
-        missing = total - passed - failed
+        missing = total - passed - failed - awaitingRD - factory
 
         with metric1:
                 st.metric("Total Request", value="", delta=total)
